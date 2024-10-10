33a93f3f70d1f6a456c83fc59a6a5ab1e9c51211
Oumezzine Farah <oumezzine.farah@yahoo.com>
2024-10-10 22:57:40 +0100

    Mongoose schema

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require ("express");
 const mongoose= require ("mongoose");
 const bodyParser= require ("body-parser");
 
+const Contact= require ("./routes/Contact");
+app.use("/api", Contact);
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+//connection from mongoose to mongoDB
+const connecttoDB= async()=> {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+            useNewUrlParser: true, // Ensures that Mongoose uses MongoDB’s new URL parser.
+            useUnifiedTopology: true //more robust connection handling
+        });
+        console.log("connected to mongoDB");
+    }catch(error){
+        console.log(error);
+    }
+}
+connecttoDB();
 
 const port= 3000;
 app.listen(port, () => {
---
+++ models/Contact.js
@@ -0,0 +1,28 @@
+const mongoose= require ("mongoose");
+
+const contactSchema= new mongoose.Schema({
+    firstName:{
+        type:String,
+        required: [true, "First Name is required" ],
+        minLength: 3,
+        maxLength: 20,
+        trim: true,
+        validate :{
+            validator: function (v) {
+                const nameRegex= /^[A-Za-z]+$/;
+                return nameRegex.test(v);
+        },
+        message : "First Name must contain only alphabetic charact"
+        
+    }
+},
+lastName:{
+type: String,
+required: [true, "Last Name is required" ],
+},
+emailAdd:{
+    type:String,
+    required:[true, "Email is required" ],
+    unique:true,
+}
+});
